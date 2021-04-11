c8b1ed2379df7d497cd3771b1483b10c1caf3d83
TungLuu <tungluu2203@kaist.ac.kr>
2021-04-11 14:57:45 +0900

    Refactor name of training script

--- train.py
+++ train.py
@@ -1,16 +1,26 @@
+import numpy as np
 import torch
 import argparse
 import os
+import math
+import gym
+import sys
+import random
 import time
 import json
 import dmc2gym
+import copy
+from tqdm import tqdm
 
 import utils
 from logger import Logger
 from video import VideoRecorder
 
 from agent.sac_ae import SacAeAgent
-
+from agent.sac_curl import SacCurlAgent
+from agent.sac_rad import SacRadAgent
+from agent.sac_cpm import SacCPMAgent
+from agent.sac_pr import SacPrAgent
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -20,12 +30,23 @@ def parse_args():
     parser.add_argument('--image_size', default=84, type=int)
     parser.add_argument('--action_repeat', default=1, type=int)
     parser.add_argument('--frame_stack', default=3, type=int)
+
+    parser.add_argument('--pre_transform_image_size', default=100, type=int)
+    parser.add_argument('--detach_encoder', default=False, action='store_true')
+    # RAD
+    parser.add_argument('--data_augs', default='crop', type=str)
+    # CURL/CPM
+    parser.add_argument('--cpc_update_freq', default=1, type=int)
+    parser.add_argument('--idm_update_freq', default=1, type=int)
+    # Physical prior
+    parser.add_argument('--use_prior', default=False, action='store_true')
     # replay buffer
     parser.add_argument('--replay_buffer_capacity', default=1000000, type=int)
     # train
     parser.add_argument('--agent', default='sac_ae', type=str)
     parser.add_argument('--init_steps', default=1000, type=int)
     parser.add_argument('--num_train_steps', default=1000000, type=int)
+    parser.add_argument('--num_train_envsteps', default=-1, type=int)
     parser.add_argument('--batch_size', default=128, type=int)
     parser.add_argument('--hidden_dim', default=1024, type=int)
     # eval
@@ -62,22 +83,44 @@ def parse_args():
     # misc
     parser.add_argument('--seed', default=1, type=int)
     parser.add_argument('--work_dir', default='.', type=str)
+    parser.add_argument('--exp', default='exp', type=str)
     parser.add_argument('--save_tb', default=False, action='store_true')
     parser.add_argument('--save_model', default=False, action='store_true')
     parser.add_argument('--save_buffer', default=False, action='store_true')
     parser.add_argument('--save_video', default=False, action='store_true')
 
+    parser.add_argument('--log_interval', default=100, type=int)
     args = parser.parse_args()
     return args
 
 
-def evaluate(env, agent, video, num_episodes, L, step):
+def evaluate(env, agent, video, num_episodes, L, step, args):
+
+    def preprocess_obs(obs):
+        if args.agent in ['sac_curl', 'sac_cpm']:
+            preprocessed = utils.center_crop_image(obs, args.image_size)  # Preprocess input for CURL
+        elif args.agent in ['sac_rad', 'sac_pr']:
+            # center crop image
+            if 'crop' in args.data_augs:
+                obs = utils.center_crop_image(obs, args.image_size)
+            if 'translate' in args.data_augs:
+                # first crop the center with pre_image_size
+                obs = utils.center_crop_image(obs, args.pre_transform_image_size)
+                # then translate cropped to center
+                obs = utils.center_translate(obs, args.image_size)
+            preprocessed = obs
+        else:
+            preprocessed = obs
+        return preprocessed
+
+    all_ep_rewards = []
     for i in range(num_episodes):
         obs = env.reset()
         video.init(enabled=(i == 0))
         done = False
         episode_reward = 0
         while not done:
+            obs = preprocess_obs(obs)
             with utils.eval_mode(agent):
                 action = agent.select_action(obs)
             obs, reward, done, _ = env.step(action)
@@ -86,11 +129,17 @@ def evaluate(env, agent, video, num_episodes, L, step):
 
         video.save('%d.mp4' % step)
         L.log('eval/episode_reward', episode_reward, step)
+        all_ep_rewards.append(episode_reward)
+
+    mean_ep_reward = np.mean(all_ep_rewards)
+    best_ep_reward = np.max(all_ep_rewards)
+    L.log('eval/mean_episode_reward', mean_ep_reward, step)
+    L.log('eval/best_episode_reward', best_ep_reward, step)
     L.dump(step)
 
 
 def make_agent(obs_shape, action_shape, args, device):
-    if args.agent == 'sac_ae':
+    if args.agent in ['sac_ae']:
         return SacAeAgent(
             obs_shape=obs_shape,
             action_shape=action_shape,
@@ -121,31 +170,215 @@ def make_agent(obs_shape, action_shape, args, device):
             num_layers=args.num_layers,
             num_filters=args.num_filters
         )
+    elif args.agent in ['sac_curl']:
+        return SacCurlAgent(
+            obs_shape=obs_shape,
+            action_shape=action_shape,
+            device=device,
+            hidden_dim=args.hidden_dim,
+            discount=args.discount,
+            init_temperature=args.init_temperature,
+            alpha_lr=args.alpha_lr,
+            alpha_beta=args.alpha_beta,
+            actor_lr=args.actor_lr,
+            actor_beta=args.actor_beta,
+            actor_log_std_min=args.actor_log_std_min,
+            actor_log_std_max=args.actor_log_std_max,
+            actor_update_freq=args.actor_update_freq,
+            critic_lr=args.critic_lr,
+            critic_beta=args.critic_beta,
+            critic_tau=args.critic_tau,
+            critic_target_update_freq=args.critic_target_update_freq,
+            encoder_type=args.encoder_type,
+            encoder_feature_dim=args.encoder_feature_dim,
+            encoder_lr=args.encoder_lr,
+            encoder_tau=args.encoder_tau,
+            num_layers=args.num_layers,
+            num_filters=args.num_filters,
+            log_interval=args.log_interval,
+            detach_encoder=args.detach_encoder,
+        )
+    elif args.agent in ['sac_cpm']:
+        return SacCPMAgent(
+            obs_shape=obs_shape,
+            action_shape=action_shape,
+            device=device,
+            hidden_dim=args.hidden_dim,
+            discount=args.discount,
+            init_temperature=args.init_temperature,
+            alpha_lr=args.alpha_lr,
+            alpha_beta=args.alpha_beta,
+            actor_lr=args.actor_lr,
+            actor_beta=args.actor_beta,
+            actor_log_std_min=args.actor_log_std_min,
+            actor_log_std_max=args.actor_log_std_max,
+            actor_update_freq=args.actor_update_freq,
+            critic_lr=args.critic_lr,
+            critic_beta=args.critic_beta,
+            critic_tau=args.critic_tau,
+            critic_target_update_freq=args.critic_target_update_freq,
+            encoder_type=args.encoder_type,
+            encoder_feature_dim=args.encoder_feature_dim,
+            encoder_lr=args.encoder_lr,
+            encoder_tau=args.encoder_tau,
+            num_layers=args.num_layers,
+            num_filters=args.num_filters,
+            log_interval=args.log_interval,
+            detach_encoder=args.detach_encoder,
+            cpc_update_freq=args.cpc_update_freq,
+            idm_update_freq=args.idm_update_freq
+        )
+    elif args.agent in ['sac_rad']:
+        return SacRadAgent(
+            obs_shape=obs_shape,
+            action_shape=action_shape,
+            device=device,
+            hidden_dim=args.hidden_dim,
+            discount=args.discount,
+            init_temperature=args.init_temperature,
+            alpha_lr=args.alpha_lr,
+            alpha_beta=args.alpha_beta,
+            actor_lr=args.actor_lr,
+            actor_beta=args.actor_beta,
+            actor_log_std_min=args.actor_log_std_min,
+            actor_log_std_max=args.actor_log_std_max,
+            actor_update_freq=args.actor_update_freq,
+            critic_lr=args.critic_lr,
+            critic_beta=args.critic_beta,
+            critic_tau=args.critic_tau,
+            critic_target_update_freq=args.critic_target_update_freq,
+            encoder_type=args.encoder_type,
+            encoder_feature_dim=args.encoder_feature_dim,
+            encoder_lr=args.encoder_lr,
+            encoder_tau=args.encoder_tau,
+            num_layers=args.num_layers,
+            num_filters=args.num_filters,
+            log_interval=args.log_interval,
+            detach_encoder=args.detach_encoder,
+            data_augs=args.data_augs
+        )
+    elif args.agent in ['sac_pr']:
+        return SacPrAgent(
+            obs_shape=obs_shape,
+            action_shape=action_shape,
+            device=device,
+            hidden_dim=args.hidden_dim,
+            discount=args.discount,
+            init_temperature=args.init_temperature,
+            alpha_lr=args.alpha_lr,
+            alpha_beta=args.alpha_beta,
+            actor_lr=args.actor_lr,
+            actor_beta=args.actor_beta,
+            actor_log_std_min=args.actor_log_std_min,
+            actor_log_std_max=args.actor_log_std_max,
+            actor_update_freq=args.actor_update_freq,
+            critic_lr=args.critic_lr,
+            critic_beta=args.critic_beta,
+            critic_tau=args.critic_tau,
+            critic_target_update_freq=args.critic_target_update_freq,
+            encoder_type=args.encoder_type,
+            encoder_feature_dim=args.encoder_feature_dim,
+            encoder_lr=args.encoder_lr,
+            encoder_tau=args.encoder_tau,
+            num_layers=args.num_layers,
+            num_filters=args.num_filters,
+            log_interval=args.log_interval,
+            detach_encoder=args.detach_encoder,
+            transition_model_type='deterministic',
+            use_prior=args.use_prior
+        )
+    else:
+        assert 'agent is not supported: %s' % args.agent
+
+
+def make_env(args, **kwargs):
+    if args.agent in ['sac_ae']:
+        return dmc2gym.make(
+            domain_name=args.domain_name,
+            task_name=args.task_name,
+            seed=args.seed,
+            visualize_reward=False,
+            from_pixels=(args.encoder_type == 'pixel'),
+            height=args.image_size,
+            width=args.image_size,
+            frame_skip=args.action_repeat
+        )
+    elif args.agent in ['sac_curl', 'sac_cpm']:
+        assert args.encoder_type == 'pixel', 'If you use state, please use `sac_ae` agent.'
+        return dmc2gym.make(
+            domain_name=args.domain_name,
+            task_name=args.task_name,
+            seed=args.seed,
+            visualize_reward=False,
+            from_pixels=(args.encoder_type == 'pixel'),
+            height=args.pre_transform_image_size,
+            width=args.pre_transform_image_size,
+            frame_skip=args.action_repeat
+        )
+    elif args.agent in ['sac_rad', 'sac_pr']:
+        assert args.encoder_type == 'pixel', 'If you use state, please use `sac_ae` agent.'
+        pre_transform_image_size = args.pre_transform_image_size if 'crop' in args.data_augs else args.image_size
+        return dmc2gym.make(
+            domain_name=args.domain_name,
+            task_name=args.task_name,
+            seed=args.seed,
+            visualize_reward=False,
+            from_pixels=(args.encoder_type == 'pixel'),
+            height=pre_transform_image_size,
+            width=pre_transform_image_size,
+            frame_skip=args.action_repeat
+        )
+    else:
+        assert 'agent is not supported: %s' % args.agent
+
+
+def make_replaybuffer(args, env, device=torch.device('cpu'), pre_aug_obs_shape=100):
+    if args.agent in ['sac_ae']:
+        return utils.ReplayBuffer(
+            obs_shape=env.observation_space.shape,
+            action_shape=env.action_space.shape,
+            capacity=args.replay_buffer_capacity,
+            batch_size=args.batch_size,
+            device=device
+        )
+    elif args.agent in ['sac_curl', 'sac_cpm']:
+        return utils.CurlReplayBuffer(
+            obs_shape=pre_aug_obs_shape,
+            action_shape=env.action_space.shape,
+            capacity=args.replay_buffer_capacity,
+            batch_size=args.batch_size,
+            device=device,
+            image_size=args.image_size,
+        )
+    elif args.agent in ['sac_rad', 'sac_pr']:
+        pre_image_size = args.pre_transform_image_size  # record the pre transform image size for translation
+        return utils.RadReplayBuffer(
+            obs_shape=pre_aug_obs_shape,
+            action_shape=env.action_space.shape,
+            capacity=args.replay_buffer_capacity,
+            batch_size=args.batch_size,
+            device=device,
+            image_size=args.image_size,
+            pre_image_size=pre_image_size,
+        )
     else:
         assert 'agent is not supported: %s' % args.agent
 
 
 def main():
     args = parse_args()
+    if args.seed == -1:
+        args.__dict__["seed"] = np.random.randint(1, 1000000)
     utils.set_seed_everywhere(args.seed)
 
-    env = dmc2gym.make(
-        domain_name=args.domain_name,
-        task_name=args.task_name,
-        seed=args.seed,
-        visualize_reward=False,
-        from_pixels=(args.encoder_type == 'pixel'),
-        height=args.image_size,
-        width=args.image_size,
-        frame_skip=args.action_repeat
-    )
+    env = make_env(args)
     env.seed(args.seed)
 
     # stack several consecutive frames together
     if args.encoder_type == 'pixel':
         env = utils.FrameStack(env, k=args.frame_stack)
 
-    utils.make_dir(args.work_dir)
+    utils.make_logdir(args)
     video_dir = utils.make_dir(os.path.join(args.work_dir, 'video'))
     model_dir = utils.make_dir(os.path.join(args.work_dir, 'model'))
     buffer_dir = utils.make_dir(os.path.join(args.work_dir, 'buffer'))
@@ -161,16 +394,22 @@ def main():
     assert env.action_space.low.min() >= -1
     assert env.action_space.high.max() <= 1
 
-    replay_buffer = utils.ReplayBuffer(
-        obs_shape=env.observation_space.shape,
-        action_shape=env.action_space.shape,
-        capacity=args.replay_buffer_capacity,
-        batch_size=args.batch_size,
-        device=device
-    )
+    # TODO: make it more neat
+    if args.agent in ['sac_curl', 'sac_cpm'] and args.encoder_type == 'pixel':
+        obs_shape = (3 * args.frame_stack, args.image_size, args.image_size)
+        pre_aug_obs_shape = (3 * args.frame_stack, args.pre_transform_image_size, args.pre_transform_image_size)
+    elif args.agent in ['sac_rad', 'sac_pr'] and args.encoder_type == 'pixel':
+        pre_transform_image_size = args.pre_transform_image_size if 'crop' in args.data_augs else args.image_size
+        obs_shape = (3 * args.frame_stack, args.image_size, args.image_size)
+        pre_aug_obs_shape = (3 * args.frame_stack, pre_transform_image_size, pre_transform_image_size)
+    else:
+        obs_shape = env.observation_space.shape
+        pre_aug_obs_shape = env.observation_space.shape
+
+    replay_buffer = make_replaybuffer(args, env, device, pre_aug_obs_shape)
 
     agent = make_agent(
-        obs_shape=env.observation_space.shape,
+        obs_shape=obs_shape,
         action_shape=env.action_space.shape,
         args=args,
         device=device
@@ -178,9 +417,14 @@ def main():
 
     L = Logger(args.work_dir, use_tb=args.save_tb)
 
+    if args.num_train_envsteps != -1:
+        # Override N training step if args.num_train_envsteps is given
+        args.num_train_steps = int(args.num_train_envsteps / args.action_repeat)
+
     episode, episode_reward, done = 0, 0, True
+    eval_freq = int(args.eval_freq / args.action_repeat)    # Freq compatible with environment steps
     start_time = time.time()
-    for step in range(args.num_train_steps):
+    for step in tqdm(range(args.num_train_steps + 1)):
         if done:
             if step > 0:
                 L.log('train/duration', time.time() - start_time, step)
@@ -188,15 +432,19 @@ def main():
                 L.dump(step)
 
             # evaluate agent periodically
-            if step % args.eval_freq == 0:
+            if step % eval_freq == 0:
+                print('[INFO] {}-{}: {} - seed: {}'.format(args.domain_name, args.task_name,
+                                                           args.exp,
+                                                           args.seed))
                 L.log('eval/episode', episode, step)
-                evaluate(env, agent, video, args.num_eval_episodes, L, step)
+                evaluate(env, agent, video, args.num_eval_episodes, L, step, args)
                 if args.save_model:
                     agent.save(model_dir, step)
                 if args.save_buffer:
                     replay_buffer.save(buffer_dir)
 
-            L.log('train/episode_reward', episode_reward, step)
+            if step % args.log_interval == 0:
+                L.log('train/episode_reward', episode_reward, step)
 
             obs = env.reset()
             done = False
@@ -204,7 +452,8 @@ def main():
             episode_step = 0
             episode += 1
 
-            L.log('train/episode', episode, step)
+            if step % args.log_interval == 0:
+                L.log('train/episode', episode, step)
 
         # sample action for data collection
         if step < args.init_steps:
